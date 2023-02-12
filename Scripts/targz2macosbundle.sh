a75c1beef0fdd78a5cd6cc526a8da80e5cfc364a
Michael Bucari-Tovo <mbucari1@gmail.com>
2023-02-12 16:28:00 -0700

    Edit Mac and Linux bundle build workflows

--- Scripts/targz2macosbundle.sh
+++ Scripts/targz2macosbundle.sh
@@ -76,6 +76,10 @@ for n in "${delfiles[@]}"; do rm "$BUNDLE_MACOS/$n"; done
 echo "Creating app bundle: $BUNDLE-$VERSION.tar.gz"
 tar -czvf "$BUNDLE-$VERSION.tar.gz" "$BUNDLE"
 
+mkdir bundle
+echo "moving to ./bundle/$BUNDLE-$VERSION.tar.gz"
+mv "$BUNDLE-$VERSION.tar.gz" "./bundle/$BUNDLE-$VERSION.tar.gz"
+
 rm -r "$BUNDLE"
 
 echo "Done!"
